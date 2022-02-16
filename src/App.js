27dd28ef6d77d8cf09fe77758bbe31e77d4a0b23
Omkar Acharekar <omkaracharekar12@gmail.com>
2022-02-17 00:27:59 +0530

    Frontend Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,163 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useState, useEffect } from "react";
+import { Bubble} from 'react-chartjs-2';
+import { Chart, registerables, ArcElement } from "chart.js";
+Chart.register(...registerables);
+Chart.register(ArcElement);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+};
+function BarChart (props)  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Bubble data={props.data} height={390} width={300} options={props.options}/>
     </div>
+  )
+}
+
+const data = {
+  datasets: [{
+    label: 'Grapefruit',
+    data: [{
+      x: 10,
+      y: 40,
+      r: 40
+    }
+  ],
+    backgroundColor: 'rgb(255, 99, 132)'
+  },
+  {
+    label: 'Lime',
+    
+    data: [{
+      x: 23,
+      y: 37,
+      r: 40
+    }
+    
+  ],
+    backgroundColor: 'rgb(0, 152, 102)'
+  },
+  {
+    label: 'Coconut',
+    data: [{
+      x: 26,
+      y: 33,
+      r: 40
+    }
+  ],
+    backgroundColor: 'rgb(0, 153, 0)'
+  },{
+    label: 'Mango',
+    data: [{
+      x: 40,
+      y: 40,
+      r: 40
+    }
+  ],
+    backgroundColor: 'rgb(255, 128, 0)'
+  }
+
+]
+};
+
+const options = {
+  scales: { x: { display: false }, y: { display: false}},
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      
+      display:false,
+    },
+  }
+}
+
+
+
+
+
+const  App = () => {
+
+
+  const [value, setValue] = useState("lime");
+  const [inputText, setInputText] = useState("");
+  const handleChange = (e) => {
+    setValue(e.target.value);
+   
+  };
+
+  function sayHello() {
+    if(inputText && value){
+  
+    callingChart(inputText,value);
+    }else{
+      alert("Insert New Choice")
+    }
+
+
+    }
+
+   function callingChart(inputText,value){
+
+
+        data.datasets.push({ label: inputText,
+        data: [{
+          x: 40,
+          y: 40,
+          r: 40
+        }
+      ],
+        backgroundColor: 'rgb(255, 128, 0)'})
+
+        console.log(data.datasets.length)
+
+        // useEffect(() => {
+        //   App();
+        // });
+
+        
+
+
+
+
+
+    }
+
+
+
+  return (
+    <div className="container">
+      <div className ="bubble">
+       
+      <BarChart data={data} options={options}/>
+      </div>
+      <div className = "setting">
+            <input type = "text" id='search' placeholder='                  NEW SUB TOPIC'  value={inputText}  onChange={(e) => setInputText(e.target.value)}/>
+
+            <div className='dropdown'>
+              <form onSubmit={handleSubmit}>
+                <label style={{fontSize:30 , fontFamily:"Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif" , color:"#fcfffe"}}>
+                  UNDER : 
+                  <select value={value} onChange={handleChange} className="SELECT">
+                    <option value="grapefruit">Grapefruit</option>
+                    <option value="lime">Lime</option>
+                    <option value="coconut">Coconut</option>
+                    <option value="mango">Mango</option>
+                  </select>
+                </label>
+              </form>
+            </div>
+           
+            <button  id='submit' onClick={sayHello}>SUBMIT</button>
+      </div>
+      
+    
+    </div>
+    
+    
+   
   );
 }
 
---
+++ src/components/Bubble.js
@@ -0,0 +1,108 @@
+// import React from 'react'
+// import { Bubble} from 'react-chartjs-2';
+// import { Chart, registerables, ArcElement } from "chart.js";
+// Chart.register(...registerables);
+// Chart.register(ArcElement);
+
+
+// function BarChart (props)  {
+//   return (
+//     <div>
+//     <Bubble data={props.data} height={390} width={300} options={props.options}/>
+//     </div>
+//   )
+// }
+
+// export default BarChart
+
+
+
+
+
+// const data = {
+//   datasets: [{
+//     label: 'Grapefruit',
+//     data: [{
+//       x: 10,
+//       y: 40,
+//       r: 40
+//     }, {
+//       x: 12,
+//       y: 39,
+//       r: 16
+//     },
+//     {
+//       x: 9,
+//       y: 38,
+//       r: 14
+//     },
+    
+    
+//   ],
+//     backgroundColor: 'rgb(255, 99, 132)'
+//   },
+//   {
+//     label: 'Lime',
+    
+//     data: [{
+//       x: 23,
+//       y: 37,
+//       r: 12
+//     }, {
+//       x: 24,
+//       y: 38,
+//       r: 13
+//     },
+//     {
+//       x: 25,
+//       y: 39,
+//       r: 40
+//     },
+    
+//   ],
+//     backgroundColor: 'rgb(0, 152, 102)'
+//   },
+//   {
+//     label: 'Coconut',
+//     data: [{
+//       x: 26,
+//       y: 33,
+//       r: 40
+//     }, {
+//       x: 28,
+//       y: 34,
+//       r: 10
+//     },
+//     {
+//       x: 30,
+//       y: 36,
+//       r: 10
+//     },
+    
+    
+//   ],
+//     backgroundColor: 'rgb(0, 153, 0)'
+//   },{
+//     label: 'Mango',
+//     data: [{
+//       x: 40,
+//       y: 40,
+//       r: 40
+//     }, {
+//       x: 41,
+//       y: 39,
+//       r: 15
+//     },
+//     {
+//       x: 42,
+//       y: 39,
+//       r: 10
+//     },
+    
+    
+//   ],
+//     backgroundColor: 'rgb(255, 128, 0)'
+//   },
+
+// ]
+// };
