278931d0994178a25c4faa5bc90256b1f1bd4a96
Omkar Acharekar <omkaracharekar12@gmail.com>
2022-02-17 13:51:37 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import './App.css';
-
 import React, { useState, useEffect } from "react";
 import { Bubble} from 'react-chartjs-2';
 import { Chart, registerables, ArcElement } from "chart.js";
 Chart.register(...registerables);
 Chart.register(ArcElement);
-
+let g = 1;
+let l = 1;
+let c = 1;
+let m = 1;
 const handleSubmit = (e) => {
   e.preventDefault();
 };
-function BarChart (props)  {
+
+
+function BarChart (props) 
+{
+  console.log(props.data);
   return (
     <div>
-    <Bubble data={props.data} height={390} width={300} options={props.options}/>
-    </div>
-  )
+    <Bubble data={props.data} height={420} width={310} options={props.options}/>
+    </div>)
 }
 
-const data = {
+
+const data = 
+{
   datasets: [{
-    label: 'Grapefruit',
-    data: [{
-      x: 10,
-      y: 40,
-      r: 40
-    }
-  ],
-    backgroundColor: 'rgb(255, 99, 132)'
-  },
+      label: 'Grapefruit',
+      data: [{
+        x: 10,
+        y: 40,
+        r: 40
+      }
+    ],
+      backgroundColor: 'rgb(255, 99, 132)'
+    },
   {
     label: 'Lime',
     
@@ -60,10 +67,13 @@ const data = {
     backgroundColor: 'rgb(255, 128, 0)'
   }
 
-]
+ ]
 };
 
-const options = {
+
+
+const options = 
+{
   scales: { x: { display: false }, y: { display: false}},
   maintainAspectRatio: false,
   plugins: {
@@ -75,78 +85,126 @@ const options = {
 }
 
 
-
-
-
 const  App = () => {
-
 
   const [value, setValue] = useState("lime");
   const [inputText, setInputText] = useState("");
+  const [chartData, setchartData] = useState(data);
+
+
   const handleChange = (e) => {
     setValue(e.target.value);
    
   };
 
-  function sayHello() {
-    if(inputText && value){
   
+  function sayHello() 
+  {
+    if(inputText && value){
     callingChart(inputText,value);
     }else{
       alert("Insert New Choice")
     }
-
-
+  }
+   
+  function callingChart(inputText, value) {
+    const newChartData = {
+      ...chartData, // shallow copy chartData object
+      datasets: chartData.datasets.slice(), // shallow copy datasets array
     }
-
-   function callingChart(inputText,value){
-
-
-        data.datasets.push({ label: inputText,
-        data: [{
-          x: 40,
-          y: 40,
-          r: 40
+  
+    if (value === "lime") {
+      const newl = [
+        {
+          x: chartData.datasets[1].data[0].x + l,
+          y: chartData.datasets[1].data[0].y + l,
+          r: chartData.datasets[1].data[0].r + l
         }
-      ],
-        backgroundColor: 'rgb(255, 128, 0)'})
-
-        console.log(data.datasets.length)
-
-        // useEffect(() => {
-        //   App();
-        // });
-
-        
-
-
-
-
-
+      ];
+      newChartData.datasets.push({
+        label: inputText,
+        data: newl,
+        backgroundColor: chartData.datasets[1].backgroundColor
+      });
+  
+      l--;
+    } else if (value === "coconut") {
+      const newc = [
+        {
+          x: chartData.datasets[2].data[0].x + c,
+          y: chartData.datasets[2].data[0].y + c,
+          r: chartData.datasets[2].data[0].r + c
+        }
+      ];
+  
+      newChartData.datasets.push({
+        label: inputText,
+        data: newc,
+        backgroundColor: chartData.datasets[2].backgroundColor
+      });
+  
+      c--;
+    } else if (value === "grapefruit") {
+      const newg = [
+        {
+          x: chartData.datasets[0].data[0].x + g,
+          y: chartData.datasets[0].data[0].y + g,
+          r: chartData.datasets[0].data[0].r + g
+        }
+      ];
+  
+      newChartData.datasets.push({
+        label: inputText,
+        data: newg,
+        backgroundColor: chartData.datasets[0].backgroundColor
+      });
+  
+      g--;
+    } else {
+      const newm = [
+        {
+          x: chartData.datasets[3].data[0].x + m,
+          y: chartData.datasets[3].data[0].y + m,
+          r: chartData.datasets[3].data[0].r + m
+        }
+      ];
+      newChartData.datasets.push({
+        label: inputText,
+        data: newm,
+        backgroundColor: chartData.datasets[3].backgroundColor
+      });
+  
+      m--;
     }
+  
+    setchartData(newChartData);
+  
+   
+  }
+    // useEffect(() => {
+      
+    // },[chartData]);
+  
 
-
-
-  return (
+   return (
     <div className="container">
-      <div className ="bubble">
-       
-      <BarChart data={data} options={options}/>
+      <div className ="bubble">  
+           <BarChart data={chartData} options={options}/>
       </div>
       <div className = "setting">
             <input type = "text" id='search' placeholder='                  NEW SUB TOPIC'  value={inputText}  onChange={(e) => setInputText(e.target.value)}/>
 
             <div className='dropdown'>
               <form onSubmit={handleSubmit}>
-                <label style={{fontSize:30 , fontFamily:"Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif" , color:"#fcfffe"}}>
-                  UNDER : 
-                  <select value={value} onChange={handleChange} className="SELECT">
+              <label style={{fontSize:30 , fontFamily:"Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif" , color:"#fcfffe"}}>
+               UNDER : 
+              <select value={value} onChange={handleChange} className="SELECT">
                     <option value="grapefruit">Grapefruit</option>
                     <option value="lime">Lime</option>
                     <option value="coconut">Coconut</option>
                     <option value="mango">Mango</option>
-                  </select>
-                </label>
+              </select>
+              </label>
               </form>
             </div>
            
