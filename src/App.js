ae838db038b39cf5b720b66361c1fbd6d72f5239
Omkar Acharekar <omkaracharekar12@gmail.com>
2022-02-18 01:09:37 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,74 +1,86 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { Bubble} from 'react-chartjs-2';
 import { Chart, registerables, ArcElement } from "chart.js";
-import back from "./img/multicolor.jpg"
+import back from "./img/multicolor.jpg";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import BarChart from './components/Bubble'
+
 Chart.register(...registerables);
+Chart.register(ChartDataLabels);
 Chart.register(ArcElement);
-let g = 1;
-let l = 1;
-let c = 1;
-let m = 1;
+
+let g = 1,l = 1,c = 1, m = 1,mm = 1;
+
 const handleSubmit = (e) => {
   e.preventDefault();
 };
 
-
-function BarChart (props) 
-{
-  console.log(props.data);
-  return (
-    <div>
-    <Bubble data={props.data} height={415} width={310} options={props.options}/>
-    </div>)
-}
+Chart.defaults.set('plugins.datalabels', {
+  color: "rgb(82, 74, 74)"
+});
 
 
 const data = 
-{
-  datasets: [{
-      label: 'Grapefruit',
+{ 
+  
+  datasets: 
+  [
+      {
+        label: 'Development',
+        data: [{
+          x: 10,
+          y: 40,
+          r: 40
+        }
+      ],
+      backgroundColor: ' #cc3300'
+      },
+
+    {
+      label: 'Stock',
+      
       data: [{
-        x: 10,
+        x: 23,
+        y: 37,
+        r: 40
+      }
+      
+    ],
+      backgroundColor: 'rgb(105, 89, 175)'
+    },
+    {
+      label: 'Product',
+      data: [{
+        x: 14,
+        y: 34,
+        r: 40
+      }
+    ],
+      backgroundColor: "#00b33c"
+    },{
+      label: 'Sales',
+      data: [{
+        x: 40,
         y: 40,
         r: 40
       }
     ],
+      backgroundColor: 'rgb(255, 128, 0)'
+    }
+    ,{
+      label: 'Marketing',
+      data: [{
+        x: 35,
+        y: 31,
+        r: 40
+      }
+    ],
+    
       backgroundColor: 'rgb(255, 99, 132)'
-    },
-  {
-    label: 'Lime',
-    
-    data: [{
-      x: 23,
-      y: 37,
-      r: 40
     }
-    
-  ],
-    backgroundColor: 'rgb(105, 89, 175)'
-  },
-  {
-    label: 'Coconut',
-    data: [{
-      x: 26,
-      y: 33,
-      r: 40
-    }
-  ],
-    backgroundColor: 'rgb(249, 236, 61)'
-  },{
-    label: 'Mango',
-    data: [{
-      x: 40,
-      y: 40,
-      r: 40
-    }
-  ],
-    backgroundColor: 'rgb(255, 128, 0)'
-  }
 
- ]
+  ]
 };
 
 
@@ -79,10 +91,38 @@ const options =
   maintainAspectRatio: false,
   plugins: {
     legend: {
-      
       display:false,
     },
-  }
+    datalabels: {
+      formatter: function(value, context) {
+        return context.dataset.label;
+      }
+      , color: 'white',
+      
+      font: {
+        weight: 'bold'
+        
+      },
+    }
+    
+  },
+  
+}
+
+
+function managevalue(chartData,index,increment){
+
+  const newl = [
+    {
+      x: chartData.datasets[index].data[0].x + increment,
+      y: chartData.datasets[index].data[0].y + increment,
+      r: chartData.datasets[index].data[0].r + increment
+    }
+  ];
+
+  return newl;
+
+
 }
 
 
@@ -93,67 +133,45 @@ const  App = () => {
   const [chartData, setchartData] = useState(data);
   const [isActive, setIsActive] = useState(false);
 
-
   const handleChange = (e) => {
     setValue(e.target.value);
    
   };
-
-  
   function sayHello() 
   {
     if(inputText && value){
     callingChart(inputText,value);
     }else{
-      alert("Insert New Choice")
+      alert("✎ ADD NEW SUB TOPIC ✎")
     }
   }
    
   function callingChart(inputText, value) {
     const newChartData = {
-      ...chartData, // shallow copy chartData object
+      ...chartData,                      // shallow copy chartData object
       datasets: chartData.datasets.slice(), // shallow copy datasets array
     }
-  
+
     if (value === "lime") {
-      const newl = [
-        {
-          x: chartData.datasets[1].data[0].x + l,
-          y: chartData.datasets[1].data[0].y + l,
-          r: chartData.datasets[1].data[0].r + l
-        }
-      ];
+      const newl =  managevalue(chartData,1,l);
       newChartData.datasets.push({
         label: inputText,
         data: newl,
         backgroundColor: chartData.datasets[1].backgroundColor
       });
   
-      l--;
+      l++;
     } else if (value === "coconut") {
-      const newc = [
-        {
-          x: chartData.datasets[2].data[0].x + c,
-          y: chartData.datasets[2].data[0].y + c,
-          r: chartData.datasets[2].data[0].r + c
-        }
-      ];
-  
+      const newc = managevalue(chartData,2,c);
       newChartData.datasets.push({
         label: inputText,
         data: newc,
         backgroundColor: chartData.datasets[2].backgroundColor
       });
   
-      c--;
+      c++;
     } else if (value === "grapefruit") {
-      const newg = [
-        {
-          x: chartData.datasets[0].data[0].x + g,
-          y: chartData.datasets[0].data[0].y + g,
-          r: chartData.datasets[0].data[0].r + g
-        }
-      ];
+      const newg = managevalue(chartData,0,g);
   
       newChartData.datasets.push({
         label: inputText,
@@ -161,31 +179,36 @@ const  App = () => {
         backgroundColor: chartData.datasets[0].backgroundColor
       });
   
-      g--;
-    } else {
-      const newm = [
-        {
-          x: chartData.datasets[3].data[0].x + m,
-          y: chartData.datasets[3].data[0].y + m,
-          r: chartData.datasets[3].data[0].r + m
-        }
-      ];
+      g++;
+    } else if(value == "mango"){
+      const newm = managevalue(chartData,3,m);
       newChartData.datasets.push({
         label: inputText,
         data: newm,
         backgroundColor: chartData.datasets[3].backgroundColor
       });
   
-      m--;
+      m++;
+    }else{
+
+      const newma = managevalue(chartData,4,mm);
+      newChartData.datasets.push({
+        label: inputText,
+        data: newma,
+        backgroundColor: chartData.datasets[4].backgroundColor
+      });
+  
+      mm++;
+
     }
   
     setchartData(newChartData);
   
    
   }
-    // useEffect(() => {
-      
-    // },[chartData]);
+
+
+    
     function handleTextChange(text) {
     
       setInputText(text)
@@ -199,7 +222,7 @@ const  App = () => {
 
    return (
     <div className="container">
-      <div className ="bubble">  
+      <div className ="bubble" style={{ backgroundImage: `url(${back})` }}>  
            <BarChart data={chartData} options={options}/>
       </div>
       <div className = "setting" style={{ backgroundImage: `url(${back})` }}>
@@ -211,13 +234,14 @@ const  App = () => {
            </div>
             <div className='dropdown'>
               <form onSubmit={handleSubmit}>
-              <label style={{fontSize:24 , fontFamily:"Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif" , color:"#fcfffe"}}>
+              <label style={{fontSize:24 , fontFamily:"Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif" , color: "rgb(80, 74, 74)"}}>
                UNDER : 
               <select value={value} onChange={handleChange} className="SELECT">
-                    <option value="grapefruit">Grapefruit</option>
-                    <option value="lime">Lime</option>
-                    <option value="coconut">Coconut</option>
-                    <option value="mango">Mango</option>
+                    <option value="grapefruit">Development</option>
+                    <option value="lime">Stock</option>
+                    <option value="coconut">Product</option>
+                    <option value="mango">Sales</option>
+                    <option value="car">Marketing</option>
               </select>
               </label>
               </form>
